98c1c720808dcd9ecfcc87dd8caac69fedde2682
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2015-12-07 10:32:42 -0500

    Adding meta viewport tag

--- client/app/bundles/Comments/components/Navigation.jsx
+++ client/app/bundles/Comments/components/Navigation.jsx
@@ -5,7 +5,9 @@ class Navigation extends React.Component{
   render(){
     return (
     	<div>
-
+	    	<head>
+	        <meta name="viewport" content="width=device-width, initial-scale=1" />
+	    	</head>
 	        <header className="navigation-bar">
 	            <a className="" href="/" id="title">Build React Native</a>
 	        </header>
